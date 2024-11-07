fb7eddbb0ed5d52cc5d68bdcaa14d2b672119680
Vasiliy Rubtsov <vasiliy.rubtsov@rtmis.ru>
2024-11-07 08:12:38 +0300

    Initial commit

---
+++ src/test/java/ru/rubtsov/vasiliy/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rubtsov.vasiliy.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
